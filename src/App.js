84d7426d64995095a0c4beb02f92d7ab4d82e38a
benrosjeremy <benrosjeremy@gmail.com>
2024-09-11 13:54:32 +0300

    update post

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Posts from "./Posts";
+import Albums from "./Albums";
 import TaskDisplay from './pages/ToDo/TaskDisplay';
 
 const Home = () => (
@@ -14,6 +15,7 @@ const Home = () => (
     <nav>
       <Link to="/posts">Posts</Link>
       <Link to="/to-do">to-do</Link>
+      <Link to="/albums">Albums</Link>
     </nav>
   </div>
 );
@@ -25,6 +27,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/posts" element={<Posts />} />
         <Route path="/to-do" element={<TaskDisplay />} />
+        <Route path="/albums" element={<Albums />} />
+
       </Routes>
     </Router>
   );
